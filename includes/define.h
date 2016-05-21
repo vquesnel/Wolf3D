d984c905f976e2ff3ab86bf131ae0298592e6af9
Victor <vquesnel@student.42.fr>
2016-05-21 21:50:16 +0200

    define.h

---
+++ includes/define.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vquesnel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/03 16:12:15 by vquesnel          #+#    #+#             */
+/*   Updated: 2016/05/17 11:32:09 by vquesnel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_H
+# define DEFINE_H
+
+# define TRUE 1
+# define FALSE 0
+# define X_SIZE 800
+# define Y_SIZE 800
+
+/*
+** KEYCODE
+*/
+
+# define ESC 53
+# define LESS_ITER 0
+# define MORE_ITER 1
+# define MORE_COLOR 24
+# define LESS_COLOR 27
+# define LEFT 123
+# define RIGHT 124
+# define UP 125
+# define DOWN 126
+# define T1 18
+# define T2 19
+# define T3 20
+# define T4 21
+# define T5 23
+# define ORIGIN 50
+# define SPACE 49
+
+/*
+** MOUSECODE
+*/
+
+# define LEFTCLICK 1
+# define ZOOMIN 4
+# define ZOOMOUT 5
+
+#endif
