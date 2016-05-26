51517452c841bba0a8a4c49c10df151603d7c2df
Victor <vquesnel@student.42.fr>
2016-05-26 23:35:38 +0200

    texture√

--- srcs/create_xpm.c
+++ srcs/create_xpm.c
@@ -6,7 +6,7 @@
 /*   By: vquesnel <vquesnel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/26 19:43:03 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/05/26 20:25:55 by vquesnel         ###   ########.fr       */
+/*   Updated: 2016/05/26 23:32:49 by vquesnel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ t_texture	*create_xpm(t_env *env)
 	tex->tab1 = (int *)mlx_get_data_addr(tex->text1, &tex->bpp, &tex->sizeline, &tex->endian);
 	tex->text2 = mlx_xpm_file_to_image(env->mlx, "./textures/colorstone.xpm", &width, &height);
 	tex->tab2 = (int*)mlx_get_data_addr(tex->text2, &tex->bpp, &tex->sizeline, &tex->endian);
-	tex->textsol = mlx_xpm_file_to_image(env->mlx, "./textures/sol.xpm", &width, &height);
-	tex->sol = (int*)mlx_get_data_addr(tex->textsol, &tex->bpp, &tex->sizeline, &tex->endian);
-	tex->textroof = mlx_xpm_file_to_image(env->mlx, "./textures/cieletoile.xpm", &width, &height);
-	tex->roof = (int*)mlx_get_data_addr(tex->textroof, &tex->bpp, &tex->sizeline, &tex->endian);
 	tex->text3 = mlx_xpm_file_to_image(env->mlx, "./textures/brique_sable64.xpm", &width, &height);
 	tex->tab3 = (int *)mlx_get_data_addr(tex->text3, &tex->bpp, &tex->sizeline, &tex->endian);
-	tex->text4 = mlx_xpm_file_to_image(env->mlx, "./textures/carrelage.xpm", &width, &height);
+	tex->text4 = mlx_xpm_file_to_image(env->mlx, "./textures/wall7.xpm", &width, &height);
 	tex->tab4 = (int *)mlx_get_data_addr(tex->text4, &tex->bpp, &tex->sizeline, &tex->endian);
+	tex->textsol = mlx_xpm_file_to_image(env->mlx, "./textures/floor3.xpm", &width, &height);
+	tex->sol = (int*)mlx_get_data_addr(tex->textsol, &tex->bpp, &tex->sizeline, &tex->endian);
+	tex->textroof = mlx_xpm_file_to_image(env->mlx, "./textures/ceil2.xpm", &width, &height);
+	tex->roof = (int*)mlx_get_data_addr(tex->textroof, &tex->bpp, &tex->sizeline, &tex->endian);
 	return (tex);
 }
