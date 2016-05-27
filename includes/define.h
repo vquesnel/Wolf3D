17322fd67b89a55390eabae95886864a58fafe1b
Victor <vquesnel@student.42.fr>
2016-05-27 18:16:07 +0200

    texture√

--- includes/define.h
+++ includes/define.h
@@ -6,17 +6,15 @@
 /*   By: vquesnel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 16:12:15 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/05/26 18:26:13 by vquesnel         ###   ########.fr       */
+/*   Updated: 2016/05/27 18:02:24 by vquesnel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINE_H
 # define DEFINE_H
 
-# define TRUE 1
-# define FALSE 0
-# define X_SIZE 800
-# define Y_SIZE 800
+# define X_SIZE 1920
+# define Y_SIZE 1080
 
 /*
 ** KEYCODE
